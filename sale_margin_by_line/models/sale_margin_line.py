2fdcf2a423559c47999ed94c99872759a0bc3de8
Angel Moya Pardo <amoyapardo@gmail.com>
2017-06-11 19:44:19 +0200

    FIX margin compute

--- sale_margin_by_line/models/sale_margin_line.py
+++ sale_margin_by_line/models/sale_margin_line.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, api, fields, _
+from odoo import models, api, fields
 
 
 class SaleOrderLine(models.Model):
@@ -9,13 +9,24 @@ class SaleOrderLine(models.Model):
     margin_percent = fields.Float(
         string='Margin %')
 
-    @api.onchange('margin_percent')
+    @api.onchange('margin_percent', 'purchase_price')
     def _onchange_margin(self):
         self.price_unit = self.purchase_price * (
             1 + (self.margin_percent / 100))
 
-    @api.onchange('price_unit', 'purchase_price')
+    @api.onchange('price_unit')
     def _onchange_price_unit(self):
         self.margin_percent = (
             (self.price_unit - self.purchase_price) * 100
         ) / self.purchase_price
+
+    @api.model
+    def create(self, values):
+        purchase_price = values.get('purchase_price')
+        price_unit = values.get('price_unit')
+        if purchase_price and price_unit:
+            margin_percent = ((price_unit - purchase_price) * 100
+                              ) / purchase_price
+            values.update({
+                'margin_percent': margin_percent})
+        return super(SaleOrderLine, self).create(values)
