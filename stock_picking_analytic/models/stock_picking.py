1eb0783da6875b8b90785a266d57851c57235477
Luis Adan JH <luis.jimenez@pesol.es>
2017-07-12 12:49:50 +0200

    WIP quant_ids vacio

--- stock_picking_analytic/models/stock_picking.py
+++ stock_picking_analytic/models/stock_picking.py
@@ -11,28 +11,25 @@ class StockPicking(models.Model):
 
     @api.multi
     def do_new_transfer(self):
+        # self.action_confirm()
+        # self.action_assign()
+        # for pack in self.pack_operation_product_ids:
+        #     if pack.product_qty > 0:
+        #         pack.write({'qty_done': pack.product_qty})
+        #     else:
+        #         pack.unlink()
+        result = super(StockPicking, self).do_new_transfer()
         if self.account_analytic_id:
-            account_analytic_obj = self.env['account.analytic.account']
             analytic_line_obj = self.env['account.analytic.line']
-            # amount = 0
-            vals = []
-            for product in self.move_lines:
-                # amount += (product.price_unit * product.product_uom_qty) * -1
-                data = analytic_line_obj.create({
+            for line in self.move_lines:
+                amount = sum(
+                    [q.location_id == line.location_dest_id and
+                     q.inventory_value for q in line.quant_ids]) * -1
+                analytic_line = analytic_line_obj.create({
                     'name': self.partner_id.name,
-                    'amount': product.product_id.lst_price,
-                    'product_id': product.product_id.id,
-                    'product_uom_id': product.product_uom.id,
+                    'amount': amount,
+                    'product_id': line.product_id.id,
+                    'product_uom_id': line.product_uom.id,
                     'account_id': self.account_analytic_id.id
                 })
-                vals.append(data)
-            # import pdb
-            # pdb.set_trace()
-            account_analytic_obj.create({
-                'name': self.partner_id.name,
-                'account_id': self.account_analytic_id.id,
-                'partner_id': self.partner_id.id,
-                'company_id': self.company_id.id,
-                'line_ids': vals
-            })
-        return super(StockPicking, self).do_new_transfer()
+        return result
