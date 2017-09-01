101a7bbc4c91cfb85bfc57725d3673a74bb7eeff
Angel Moya Pardo <amoyapardo@gmail.com>
2017-09-01 09:48:38 +0200

    IMP custom

---
+++ dta_customize/models/purchase.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from odoo import models, api
+
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    @api.multi
+    def action_rfq_send(self):
+        result = super(PurchaseOrder, self).action_rfq_send()
+        template_obj = self.env['mail.template']
+        try:
+            if self.env.context.get('send_rfq', False):
+                template = template_obj.search(
+                    [('name', '=', 'DTA - Pedido de compra')])
+            else:
+                template = template_obj.search(
+                    [('name', '=', 'DTA - Pedido de compra')])
+        except:
+            template = False
+        if template:
+            template_id = template[0].id
+            result['context'].update({
+                'default_template_id': template_id,
+            })
+        return result
