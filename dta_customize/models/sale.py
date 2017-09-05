b64359314236ec93f8201bdc6e58b49c35883e73
Angel Moya Pardo <amoyapardo@gmail.com>
2017-09-05 18:22:59 +0200

    IMP sale email

---
+++ dta_customize/models/sale.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from odoo import models, api
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def action_quotation_send(self):
+        result = super(SaleOrder, self).action_quotation_send()
+        template_obj = self.env['mail.template']
+        try:
+            template = template_obj.search(
+                    [('name', '=', 'DTA - Presupuesto venta')])
+        except:
+            template = False
+        if template:
+            template_id = template[0].id
+            result['context'].update({
+                'default_template_id': template_id,
+            })
+        return result
