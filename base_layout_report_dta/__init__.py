511389aea6078c132c6e249709d0f0714afa71c7
Angel Moya Pardo <amoyapardo@gmail.com>
2017-10-17 17:41:08 +0200

    I RM module

--- base_layout_report_dta/__init__.py
+++
@@ -1,2 +0,0 @@
-# encoding: utf-8
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
--- sale_order_report_dta/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# encoding: utf-8
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from . import sale_order_line
